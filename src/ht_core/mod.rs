c02f2bda9826edfac873d31cd366668782b1b8fe
Yaulendil <jsdreim@gmail.com>
2019-12-20 21:55:17 -0500

    Partially implement most core Functions

--- src/ht_core/mod.rs
+++ src/ht_core/mod.rs
@@ -1,16 +1,65 @@
 mod ircv3;
 
-use hexchat_plugin::{EventAttrs, PluginHandle, Word, WordEol};
+use hexchat_plugin::{EAT_NONE, EventAttrs, PluginHandle, Word, WordEol};
 use ircv3::{Message, split};
 
 
-pub fn cb_server(
-    ph: &mut PluginHandle, _word: Word, _word_eol: WordEol, attr: EventAttrs,
+pub struct Sponge<'a> {
+    pub value: Option<Message<'a>>,
+}
+
+impl<'a> Sponge<'a> {
+    pub fn put(&mut self, _ph: &mut PluginHandle, line: &'a str) {
+        let new: Message = split(line);
+        self.value = Some(new);
+    }
+
+    pub fn pop(&mut self, signature: &str) -> Option<Message> {
+        match &self.value {
+            None => None,  // If we have no Message, return None.
+            Some(msg) => {
+                // If we have a Message...
+                if msg.prefix == signature {
+                    // ...and the Signature matches, delete Value.
+                    self.value = None;
+                    // And then, return the Message.
+                    Some(msg)  // FIXME
+                } else {
+                    // Otherwise, keep the Message and return None.
+                    None
+                }
+            }
+        }
+    }
+}
+
+
+pub static mut CURRENT: Sponge = Sponge {
+    value: None,
+};
+
+
+pub fn cb_print(
+    _ph: &mut PluginHandle, _word: Word,
 ) -> hexchat_plugin::Eat {
-    ph.print("RECEIVING");
-    ph.print(attr.tags);  // DEBUG
+    //  TODO:
+    //  Make signature
+    //  event = CURRENT.pop(signature)
+    //  if event:
+    //      re-emit Print with User Badges, etc.
+    //      EAT_ALL
 
-    let msg: Message = split(attr.tags);
+    EAT_NONE
+}
 
-    hexchat_plugin::EAT_NONE
+
+pub fn cb_server(
+    _ph: &mut PluginHandle, _word: Word, _word_eol: WordEol, attr: EventAttrs,
+) -> hexchat_plugin::Eat {
+//    let msg: Message = split(&attr.tags.as_str());
+    //  TODO:
+    //  if msg.command != "PRIVMSG":
+    //      Check for Subscriptions, etc.
+
+    EAT_NONE
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,9 @@ extern crate hexchat_plugin;
 
 mod ht_core;
 
-use hexchat_plugin::{CommandHookHandle, Plugin, PluginHandle, ServerHookHandle};
+use hexchat_plugin::{
+    CommandHookHandle, EventAttrs, Plugin, PluginHandle, PrintHookHandle, ServerHookHandle
+};
 use std::sync::Mutex;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
@@ -19,26 +21,54 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 #[derive(Default)]
 struct HexTwitch {
     cmd: Mutex<Option<CommandHookHandle>>,
+    print: Mutex<Option<PrintHookHandle>>,
     servmsg: Mutex<Option<ServerHookHandle>>,
+    servmsg_2: Mutex<Option<ServerHookHandle>>,
 }
 
 impl Plugin for HexTwitch {
     fn init(&self, ph: &mut PluginHandle, _arg: Option<&str>) -> bool {
         ph.register(NAME, DESC, VERSION);
 
+        // Set Command ASDF to print "qwert" to sanity-check that we are loaded.
         *self.cmd.lock().unwrap() = Some(ph.hook_command(
             "asdf",
+
             |ph, _arg, _arg_eol| {
                 ph.print("qwert");
                 hexchat_plugin::EAT_ALL
             },
+
             hexchat_plugin::PRI_NORM,
             Some("prints 'qwert'"),
         ));
 
-        // Copied and modified the above for this line. Dont understand it. At all.
+        // Hook Print Events into Handler.
+        *self.print.lock().unwrap() = Some(ph.hook_print(
+            "Channel Message",  // Catch all events.
+            ht_core::cb_print,  // Send to Server Callback.
+            hexchat_plugin::PRI_NORM,  // Normal Priority.
+        ));
+
+        // Hook PRIVMSG Server Messages into Store Method of Current Message.
         *self.servmsg.lock().unwrap() = Some(ph.hook_server_attrs(
-            "PRIVMSG",  // Catch all events.
+            "PRIVMSG",  // Catch PRIVMSG events; Chat Messages.
+
+            |ph: &mut PluginHandle, _word, _word_eol, attr: EventAttrs| {
+
+                // Send to `CURRENT.store()`.
+                unsafe { ht_core::CURRENT.put(ph, attr.tags.as_str()) };
+                // FIXME: `attr.tags` does not live long enough
+
+                hexchat_plugin::EAT_NONE
+            },
+
+            hexchat_plugin::PRI_NORM,  // Normal Priority.
+        ));
+
+        // Hook RAW LINE Server Messages into the general Handler Callback.
+        *self.servmsg_2.lock().unwrap() = Some(ph.hook_server_attrs(
+            "RAW LINE",  // Catch all events.
             ht_core::cb_server,  // Send to Server Callback.
             hexchat_plugin::PRI_NORM,  // Normal Priority.
         ));
