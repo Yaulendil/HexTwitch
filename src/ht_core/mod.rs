d87681d502b4b05fc6d90b96b2cf6884095b6b72
Yaulendil <jsdreim@gmail.com>
2020-02-11 21:40:32 -0500

    Improve Network assurance

--- src/ht_core/mod.rs
+++ src/ht_core/mod.rs
@@ -45,18 +45,35 @@ safe_static! {
 }
 
 
-pub fn cb_print(_word: &[String], dt: DateTime<Utc>) -> EatMode {
-    //  FIXME
-    let sig: &str = "asdf";
-    //  FIXME
+//macro_rules! if_twitch {
+//    {$b:block} => {
+//        match get_network_name() {
+//            Some(network) if network.eq_ignore_ascii_case("twitch") => {
+//                $b
+//            }
+//            _ => EatMode::None,
+//        }
+//    }
+//}
 
-    match CURRENT.lock().unwrap().pop(sig) {
-        None => EatMode::None,
-        Some(msg) => {
-            //  TODO: Re-emit Print with User Badges, etc.
-//            EatMode::All
-            EatMode::None
+
+pub fn cb_print(_word: &[String], dt: DateTime<Utc>) -> EatMode {
+    match get_network_name() {
+        Some(network) if network.eq_ignore_ascii_case("twitch") => {
+            //  FIXME
+            let sig: &str = "asdf";
+            //  FIXME
+
+            match CURRENT.lock().unwrap().pop(sig) {
+                None => EatMode::None,
+                Some(msg) => {
+                    //  TODO: Re-emit Print with User Badges, etc.
+//                    EatMode::All
+                    EatMode::None
+                }
+            }
         }
+        _ => EatMode::None,
     }
 }
 
@@ -64,16 +81,14 @@ pub fn cb_print(_word: &[String], dt: DateTime<Utc>) -> EatMode {
 /// Handle a Server Message, received by the Hook for "RAW LINE".
 pub fn cb_server(_word: &[String], dt: DateTime<Utc>, raw: String) -> EatMode {
     match get_network_name() {
-        Some(network) if {
-            (&network == "Twitch") || (&network.to_lowercase() == "twitch")
-        } => {
+        Some(network) if network.eq_ignore_ascii_case("twitch") => {
             let msg: Message = Message::new(&raw);
             match msg.command.as_str() {
                 //  Chat Messages.
                 "PRIVMSG" => {
                     CURRENT.lock().unwrap().put(msg);
                     EatMode::None
-                },
+                }
                 "WHISPER" => events::whisper(msg),
 
                 "ROOMSTATE" => EatMode::Hexchat,
