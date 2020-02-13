1b464fbf704bec75d96156bb50ffdc85963d8bf3
Yaulendil <jsdreim@gmail.com>
2020-02-12 21:22:12 -0500

    <???>

--- src/lib.rs
+++ src/lib.rs
@@ -32,15 +32,15 @@ use hexchat::{
 };
 
 
-enum Hook {
-    CommandHook(Command),
-    PrintHook(PrintEventListener),
-    ServerHook(RawServerEventListener),
-}
+//enum Hook {
+//    CommandHook(Command),
+//    PrintHook(PrintEventListener),
+//    ServerHook(RawServerEventListener),
+//}
 
 
 #[derive(Default)]
-struct HexTwitch { hooks: Mutex<Vec<Hook>> }
+struct HexTwitch { /*hooks: Mutex<Vec<Hook>>*/ }
 
 impl Plugin for HexTwitch {
     const NAME: &'static str = env!("CARGO_PKG_NAME");
@@ -48,8 +48,11 @@ impl Plugin for HexTwitch {
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
     fn new() -> Self {
+        print_plain(&format!("Loading {} {}", Self::NAME, Self::VERSION));
+
         // Set Command ASDF to print "qwert" to sanity-check that we are loaded.
-        let cmd = Hook::CommandHook(register_command(
+//        let cmd = Hook::CommandHook(
+        register_command(
             "asdf",
             "prints 'qwert'",
             Priority::NORMAL,
@@ -57,39 +60,44 @@ impl Plugin for HexTwitch {
                 print_plain("qwert");
                 EatMode::All
             },
-        ));
+        );
 
         // Hook Print Events into Handler.
-        let print = Hook::PrintHook(add_print_event_listener(
+//        let print = Hook::PrintHook(
+        add_print_event_listener(
             PrintEvent::CHANNEL_MESSAGE,  // Catch Message Events.
             Priority::NORMAL,
             ht_core::cb_print,  // Send to Print Callback.
-        ));
+        );
 
         // Hook RAW LINE Server Messages into the general Handler Callback.
-        let servmsg = Hook::ServerHook(add_raw_server_event_listener(
+//        let servmsg = Hook::ServerHook(
+        add_raw_server_event_listener(
             "RAW LINE",  // Catch all events.
             Priority::NORMAL,
             ht_core::cb_server,  // Send to Server Callback.
-        ));
+        );
 
-        Self { hooks: Mutex::new(vec![cmd, print, servmsg]) }
+        let new: Self = Self { /*hooks: Mutex::new(vec![cmd, print, servmsg])*/ };
+
+        print_plain(&format!("{} loaded.", Self::NAME));
+        new
     }
 }
 
 
-impl Drop for HexTwitch {
-    fn drop(&mut self) {
-        let iter: Vec<Hook> = replace(self.hooks.get_mut().unwrap(), vec![]);
-        for hopt in iter {
-            match hopt {
-                Hook::CommandHook(handle) => { deregister_command(handle) }
-                Hook::PrintHook(handle) => { remove_print_event_listener(handle) }
-                Hook::ServerHook(handle) => { remove_raw_server_event_listener(handle) }
-            }
-        }
-    }
-}
+//impl Drop for HexTwitch {
+//    fn drop(&mut self) {
+//        let iter: Vec<Hook> = replace(self.hooks.get_mut().unwrap(), vec![]);
+//        for hopt in iter {
+//            match hopt {
+//                Hook::CommandHook(handle) => { deregister_command(handle) }
+//                Hook::PrintHook(handle) => { remove_print_event_listener(handle) }
+//                Hook::ServerHook(handle) => { remove_raw_server_event_listener(handle) }
+//            }
+//        }
+//    }
+//}
 
 
 plugin!(HexTwitch);
