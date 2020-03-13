593ef885e4d2d285b3f5a3781f7e2949bba3fe5e
Yaulendil <jsdreim@gmail.com>
2020-03-13 19:51:07 -0400

    Break apart Print Callback

--- src/ht_core/mod.rs
+++ src/ht_core/mod.rs
@@ -57,6 +57,138 @@ safe_static! {
 }
 
 
+fn check_message(channel: &str, author: &str) -> Option<Message> {
+    let sig: &str = &format!(
+        "{}:{}",
+        &channel,
+        strip_formatting(author).unwrap_or_default(),
+    );
+
+    let mut guard = CURRENT.lock();
+    let msg_maybe = guard.pop(sig);
+    drop(guard);
+
+    msg_maybe
+}
+
+
+/// Message comes from Server. IRC Representation available.
+fn print_with_irc(channel: &str, etype: PrintEvent, word: &[String], msg: Message) -> EatMode {
+    if let Some(tags) = &msg.tags {
+        if let Some(bits) = tags.get("bits") {
+            if let Ok(n) = bits.parse::<usize>() {
+                events::cheer(&msg.author.display_name(), n);
+            }
+        }
+
+        if let Some(custom) = tags.get("custom-reward-id") {
+            //  This Message is a Custom Reward.
+            if let Some(notif) = events::reward(custom) {
+                //  We know what it should be called.
+                echo(
+                    EVENT_REWARD,
+                    &[
+                        notif,
+                        &format!("{}:", &msg.author.display_name()),
+                        &word[1].as_str(),
+                    ],
+                    2,
+                );
+            } else {
+                //  We do NOT know what it should be called. Use a
+                //      generic "CUSTOM" label, and also print the
+                //      ID.
+                echo(
+                    EVENT_REWARD,
+                    &[
+                        "CUSTOM",
+                        &format!("({}) {}:", custom, &msg.author.display_name()),
+                        &word[1].as_str(),
+                    ],
+                    2,
+                );
+            }
+
+            return EatMode::All;
+        } else if tags.get("msg-id")
+            .unwrap_or(&String::new())
+            == "highlighted-message" {
+            echo(
+                EVENT_ALERT,
+                &[
+                    &msg.author.display_name(),
+                    &word[1].as_str(),
+                ],
+                2,
+            );
+
+            return EatMode::All;
+        }
+    }
+
+    match etype {
+        PrintEvent::YOUR_MESSAGE
+        | PrintEvent::YOUR_ACTION
+        => {
+            let badge_str: String = USERSTATE.read().get(&channel).to_string();
+            echo(etype, &[&word[0] as &str, &word[1], "_", &badge_str], 2);
+
+            EatMode::All
+        }
+        PrintEvent::CHANNEL_MESSAGE
+        | PrintEvent::CHANNEL_ACTION
+        | PrintEvent::CHANNEL_MSG_HILIGHT
+        | PrintEvent::CHANNEL_ACTION_HILIGHT
+        => {
+            let badges = Badges::new(
+                &msg.get_tag("badges").unwrap_or_default()
+            );
+            echo(
+                etype,
+                &[&word[0] as &str, &word[1], "", &badges.output],
+                {
+                    if etype == PrintEvent::CHANNEL_MSG_HILIGHT
+                        || etype == PrintEvent::CHANNEL_ACTION_HILIGHT
+                    { 3 } else { 2 }
+                },
+            );
+
+            send_command(&format!(
+                "RECV :{0}!{0}@twitch.tv/{0} JOIN {1}",
+                msg.author.user.to_ascii_lowercase(),
+                channel,
+            ));
+
+            EatMode::All
+        }
+        _ => EatMode::None
+    }
+}
+
+
+/// No IRC Representation available for Message.
+fn print_without_irc(channel: &str, etype: PrintEvent, word: &[String]) -> EatMode {
+    if !channel.starts_with("#") {
+        //  User has spoken inside a Whisper Tab. We must take
+        //      the message typed, and forward it to the Whisper
+        //      Command via ".w {}".
+        events::whisper_send(etype, &channel, word);
+
+        EatMode::All
+    } else if &word[2] == "" {
+        //  User has spoken in a normal Channel, but has no Badges.
+        //      Add the Badges from the User State and re-emit.
+        let badge_str: String = USERSTATE.read().get(&channel).to_string();
+        echo(etype, &[&word[0] as &str, &word[1], "_", &badge_str], 2);
+
+        EatMode::All
+    } else {
+        //  This is a re-emit. Do nothing.
+        EatMode::None
+    }
+}
+
+
 /// Reset the Color of a newly-focused Tab.
 pub(crate) fn cb_focus(_channel: ChannelRef) -> EatMode {
     TABCOLORS.write().reset();
@@ -82,126 +214,17 @@ pub(crate) fn cb_print(etype: PrintEvent, word: &[String]) -> EatMode {
     let channel = get_channel_name();
     match get_network_name() {
         Some(network) if network.eq_ignore_ascii_case("twitch") => {
-            let sig: &str = &format!(
-                "{}:{}",
-                &channel,
-                strip_formatting(&word[0]).unwrap_or_default(),
-            );
-
-            let mut guard = CURRENT.lock();
-            let msg_maybe = guard.pop(sig);
-            drop(guard);
-
-            if let Some(msg) = msg_maybe {
+            if let Some(msg) = check_message(&channel, &word[0]) {
                 //  Message comes from Server. IRC Representation available.
-
-                if let Some(tags) = &msg.tags {
-                    if let Some(bits) = tags.get("bits") {
-                        if let Ok(n) = bits.parse::<usize>() {
-                            events::cheer(&msg.author.display_name(), n);
-                        }
-                    }
-
-                    if let Some(custom) = tags.get("custom-reward-id") {
-                        if let Some(notif) = events::reward(custom) {
-                            echo(
-                                EVENT_REWARD,
-                                &[
-                                    notif,
-                                    &format!("{}:", &msg.author.display_name()),
-                                    &word[1].as_str(),
-                                ],
-                                2,
-                            );
-                        } else {
-                            echo(
-                                EVENT_REWARD,
-                                &[
-                                    "CUSTOM",
-                                    &format!("({}) {}:", custom, &msg.author.display_name()),
-                                    &word[1].as_str(),
-                                ],
-                                2,
-                            );
-                        }
-
-                        return EatMode::All;
-                    } else if tags.get("msg-id")
-                        .unwrap_or(&String::new())
-                        == "highlighted-message" {
-                        echo(
-                            EVENT_ALERT,
-                            &[
-                                &msg.author.display_name(),
-                                &word[1].as_str(),
-                            ],
-                            2,
-                        );
-
-                        return EatMode::All;
-                    }
-                }
-
-                match etype {
-                    PrintEvent::YOUR_MESSAGE
-                    | PrintEvent::YOUR_ACTION
-                    => {
-                        let badge_str: String = USERSTATE.read().get(&channel).to_string();
-                        echo(etype, &[&word[0] as &str, &word[1], "_", &badge_str], 2);
-
-                        EatMode::All
-                    }
-                    PrintEvent::CHANNEL_MESSAGE
-                    | PrintEvent::CHANNEL_ACTION
-                    | PrintEvent::CHANNEL_MSG_HILIGHT
-                    | PrintEvent::CHANNEL_ACTION_HILIGHT
-                    => {
-                        let badges = Badges::new(
-                            &msg.get_tag("badges").unwrap_or_default()
-                        );
-                        echo(
-                            etype,
-                            &[&word[0] as &str, &word[1], "", &badges.output],
-                            {
-                                if etype == PrintEvent::CHANNEL_MSG_HILIGHT
-                                    || etype == PrintEvent::CHANNEL_ACTION_HILIGHT
-                                { 3 } else { 2 }
-                            },
-                        );
-
-                        send_command(&format!(
-                            "RECV :{0}!{0}@twitch.tv/{0} JOIN {1}",
-                            msg.author.user.to_ascii_lowercase(),
-                            channel,
-                        ));
-
-                        EatMode::All
-                    }
-                    _ => EatMode::None
-                }
+                print_with_irc(&channel, etype, word, msg)
             } else if let PrintEvent::YOUR_MESSAGE | PrintEvent::YOUR_ACTION = etype {
                 //  No IRC Representation available for Message.
-                if !channel.starts_with("#") {
-                    //  User has spoken inside a Whisper Tab. We must take
-                    //      the message typed, and forward it to the Whisper
-                    //      Command via ".w {}".
-                    events::whisper_send(etype, &channel, word);
-
-                    EatMode::All
-                } else if &word[2] == "" {
-                    //  User has spoken in a normal Channel, but has no Badges.
-                    //      Add the Badges from the User State and re-emit.
-                    let badge_str: String = USERSTATE.read().get(&channel).to_string();
-                    echo(etype, &[&word[0] as &str, &word[1], "_", &badge_str], 2);
-
-                    EatMode::All
-                } else {
-                    //  This is a re-emit. Do nothing.
-                    EatMode::None
-                }
-            } else { EatMode::None }
+                print_without_irc(&channel, etype, word)
+            } else {
+                EatMode::None
+            }
         }
-        _ => EatMode::None,
+        _ => EatMode::None
     }
 }
 
